4bbd4e2557aa1a28e8a1d71143a219f1cfd8542a
Umuhae <konlp@kakao.com>
2020-12-31 03:12:49 +0900

    [*]

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG'))
+DEBUG = True
 if not DEBUG:
     ALLOWED_HOSTS ='*'
 else:
